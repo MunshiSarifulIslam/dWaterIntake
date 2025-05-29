313c11e289367f41d85d4a051146bb009023d723
Munshi Sariful Islam <munshsarif176@gmail.com>
2025-05-30 00:50:15 +0530

    Initial Commit

---
+++ dWaterIntake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dWaterIntake
+//
+//  Created by Munshi Sariful Islam on 30/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dWaterIntake/dWaterIntakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dWaterIntakeApp.swift
+//  dWaterIntake
+//
+//  Created by Munshi Sariful Islam on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct dWaterIntakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
